02f52e143b83f3fcd4951054c209f01fb24a310f
james6521 <49067631+james6521@users.noreply.github.com>
2021-06-03 19:06:46 +0100

    removed ARN

--- provider copy.tf
+++ provider copy.tf
@@ -14,8 +14,5 @@ provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
 
-  assume_role {
-    # The role ARN within Account B to AssumeRole into.
-    role_arn = "arn:aws:iam::${local.vpc_options.aws_account}:role/OrganizationAccountAccessRole"
-  }
+  
 }
